59f7ff875bb83509b0ea2696866bd0d857369266
danielsones <daniel.sones123@gmail.com>
2026-06-05 18:49:40 +0100

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -25,25 +25,46 @@ func main() {
 			continue
 		}
 
-		if input == "exit" {
+		parts := strings.Split(input, " ")
+		command := parts[0]
+		args := parts[1:]
+
+		switch command {
+		case "exit":
 			os.Exit(0)
-		}
-		if strings.HasPrefix(input, "echo ") {
-			fmt.Println(strings.TrimPrefix(input, "echo "))
-		}
-		if strings.HasPrefix(input, "type ") {
-			postFix := strings.TrimPrefix(input, "type ")
-			if slices.Contains(builtIns, postFix) {
-				fmt.Printf("%s is a shell builtin \n", postFix)
-			} else if path, _ := exec.LookPath(postFix); path != "" {
-				fmt.Printf("%s is %s\n", postFix, path)
+		case "echo":
+			fmt.Println(strings.Join(args, " "))
+		case "type":
+			if slices.Contains(builtIns, args[0]) {
+				fmt.Printf("%s is a shell builtin \n", args[0])
+			} else if path, _ := exec.LookPath(args[0]); path != "" {
+				fmt.Printf("%s is %s\n", args[0], path)
 			} else {
-				fmt.Printf("%s: not found\n", postFix)
+				fmt.Printf("%s: not found\n", args[0])
 			}
-		}
-		if !slices.Contains(builtIns, strings.Split(input, " ")[0]) {
+		default:
 			fmt.Printf("%s: command not found \n", input)
 		}
-	}
+
+	//	if input == "exit" {
+	//		os.Exit(0)
+	//	}
+	//	if strings.HasPrefix(input, "echo ") {
+	//		fmt.Println(strings.TrimPrefix(input, "echo "))
+	//	}
+	//	if strings.HasPrefix(input, "type ") {
+	//		postFix := strings.TrimPrefix(input, "type ")
+	//		if slices.Contains(builtIns, postFix) {
+	//			fmt.Printf("%s is a shell builtin \n", postFix)
+	//		} else if path, _ := exec.LookPath(postFix); path != "" {
+	//			fmt.Printf("%s is %s\n", postFix, path)
+	//		} else {
+	//			fmt.Printf("%s: not found\n", postFix)
+	//		}
+	//	}
+	//	if !slices.Contains(builtIns, strings.Split(input, " ")[0]) {
+	//		fmt.Printf("%s: command not found \n", input)
+	//	}
+	//}
 
 }
